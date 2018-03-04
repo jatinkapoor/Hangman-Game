a5ed7198aabaae6304229b9c92955a8d5d71e1db
Jatin.Kapoor <jatin.kapoor@target.com>
2018-03-04 15:49:06 -0600

    refactored cleaner version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,126 +1,114 @@
 'use-strict'
 
-const words = ['exciting', 'beautiful', 'became', 'eventful', 'actually']
-let word = words[Math.floor(Math.random() * words.length)];
-
-let winElement = document.getElementById("wins");
-let attempsElement = document.getElementById("attempts");
-let keyPressArray = [];
-let wins = 0;
-let hasWon = false;
-let losses;
-let attempts = 10;
-//console.log(word);
-
-const createPlaceholderForWord = function (word) {
-  let target = document.getElementById("target");
-
-  while (target.firstChild) {
-    target.removeChild(target.firstChild);
-  }
-
-  for (let i = 0; i < word.length; i++) {
-    var node = document.createElement("span");
-    var textnode = document.createTextNode(" _ ");
-    node.appendChild(textnode);
-    node.setAttribute("id", `letter${i}`);
-    target.appendChild(node);
-  }
-
-  winElement.innerText = wins;
-  attempsElement.innerText = attempts;
-};
-
-const replacePlaceHolderWithLetter = function (word, keyPressed) {
-
-  if (word.includes(keyPressed)) {
-    let arr = [];
+let game = {
+  words: ['exciting', 'beautiful', 'became', 'eventful', 'actually',
+    'overdone', 'prefect', 'crayers', 'behowl', 'tired', 'empathy', 'unsteeled'
+  ],
+  word: '',
+  winElement: document.getElementById("wins"),
+  attempsElement: document.getElementById("attempts"),
+  keyPressArray: [],
+  keyPressLocation: document.getElementById("keyArray"),
+  wins: 0,
+  hasWon: false,
+  attempts: 10,
+  createPlaceholderForWord: (word) => {
+    let target = document.getElementById("target");
+    while (target.firstChild) {
+      target.removeChild(target.firstChild);
+    }
     for (let i = 0; i < word.length; i++) {
-      if (word[i] === keyPressed) {
-        arr.push(i);
+      let node = document.createElement("span");
+      let textnode = document.createTextNode(" _ ");
+      node.appendChild(textnode);
+      node.setAttribute("id", `letter${i}`);
+      target.appendChild(node);
+    }
+    game.winElement.innerText = game.wins;
+    game.attempsElement.innerText = game.attempts;
+  },
+  replacePlaceHolderWithLetter: (word, keyPressed) => {
+    if (word.includes(keyPressed)) {
+      let arr = [];
+      for (let i = 0; i < word.length; i++) {
+        if (word[i] === keyPressed) {
+          arr.push(i);
+        }
       }
+      for (let i = 0; i < arr.length; i++) {
+        let element = document.getElementById(`letter${arr[i]}`);
+        element.innerHTML = keyPressed;
+      }
+    } else {
+      game.attempts = game.attempts - 1;
+      game.attempsElement.innerText = game.attempts;
     }
-    for (let i = 0; i < arr.length; i++) {
-      let element = document.getElementById(`letter${arr[i]}`);
-      element.innerHTML = keyPressed;
+  },
+  keepTrackofLetterUsed: (keyPressed) => {
+    game.keyPressArray.push(keyPressed);
+    const keyPressLocation = game.keyPressLocation;
+    let node = document.createElement("span");
+    let textnode = document.createTextNode(keyPressed);
+    node.appendChild(textnode);
+    keyPressLocation.appendChild(node);
+  },
+  checkForWin: (keyPressArray, word) => {
+    let wordArray = Array.from(word);
+    let ret = wordArray.every((val) => keyPressArray.indexOf(val) >= 0);
+    if (ret === true) {
+      let winElement = game.winElement;
+      game.wins = game.wins + 1;
+      game.hasWon = true;
     }
-  } else {
-    attempts = attempts - 1;
-    attempsElement.innerText = attempts;
+  },
+  reset: () => {
+    game.hasWon = false;
+    game.attempts = 10;
+    while (game.keyPressArray.length !== 0) {
+      game.keyPressArray.pop();
+    }
+    let target = document.getElementById("keyArray");
+    while (target.firstChild) {
+      target.removeChild(target.firstChild);
+    }
+    let word = game.words[Math.floor(Math.random() * game.words.length)];
+    game.word = word;
+    game.createPlaceholderForWord(game.word);
+  },
+  gameFunction: (keyPressed) => {
+    game.keepTrackofLetterUsed(keyPressed);
+    game.replacePlaceHolderWithLetter(game.word, keyPressed);
+    game.checkForWin(game.keyPressArray, game.word);
+    if (game.hasWon === true) {
+      game.winElement.innerText = game.wins;
+      togglePlay();
+      game.reset();
+    }
   }
-};
-
-const keepTrackofLetterUsed = function (keyPressed) {
-  keyPressArray.push(keyPressed);
-  const keyPressLocation = document.getElementById("keyArray");
-  var node = document.createElement("span");
-  var textnode = document.createTextNode(keyPressed);
-  node.appendChild(textnode);
-  keyPressLocation.appendChild(node);
-};
-
-const checkForWin = function (keyPressArray, word) {
-  let wordArray = Array.from(word);
-  let ret = wordArray.every((val) => keyPressArray.indexOf(val) >= 0);
-  if (ret === true) {
-    let winElement = document.getElementById("wins");
-    wins = wins + 1;
-    hasWon = true;
-  }
-};
-
-const reset = function () {
-  hasWon = false;
-  attempts = 10;
-  //console.log("In reset");
-
-  //console.log(keyPressArray);
-  while (keyPressArray.length !== 0) {
-    keyPressArray.pop();
-  }
-
-  //console.log(keyPressArray);
-  let target = document.getElementById("keyArray");
-  while (target.firstChild) {
-    target.removeChild(target.firstChild);
-  }
-  word = words[Math.floor(Math.random() * words.length)];
-  //console.log(word);
-  createPlaceholderForWord(word);
-
 }
 
-createPlaceholderForWord(word);
+game.word = game.words[Math.floor(Math.random() * game.words.length)];
+game.createPlaceholderForWord(game.word);
 
 document.onkeypress = ((event) => {
 
   let keyPressed = event.key.toLowerCase();
-
-  if (attempts === 0) {
-    const keyPressLocation = document.getElementById("keyArray");
-    keyPressLocation.innerText = "game over !! press 'enter'"
+  if (game.attempts === 0) {
+    let keyPressLocation = game.keyPressLocation;
+    keyPressLocation.innerText = "Game Over !! Press Enter";
     if (keyPressed === "enter") {
-      reset();
+      game.reset();
     }
   } else {
-    if (keyPressArray.length === 0) {
-      keepTrackofLetterUsed(keyPressed);
-      replacePlaceHolderWithLetter(word, keyPressed);
-      checkForWin(keyPressArray, word);
-      if (hasWon === true) {
-        winElement.innerText = wins;
-        reset();
-      }
-
-    } else if (keyPressArray.includes(keyPressed) === false) {
-      keepTrackofLetterUsed(keyPressed);
-      replacePlaceHolderWithLetter(word, keyPressed);
-      checkForWin(keyPressArray, word);
-      //console.log(wins)
-      if (hasWon === true) {
-        winElement.innerText = wins;
-        reset();
-      }
+    if (game.keyPressArray.length === 0) {
+      game.gameFunction(keyPressed);
+    } else if (game.keyPressArray.includes(keyPressed) === false) {
+      game.gameFunction(keyPressed);
     }
   }
 });
+
+function togglePlay() {
+  let audioElement = document.getElementById('audio');
+  return audioElement.paused ? audioElement.play() : audioElement.pause();
+};
